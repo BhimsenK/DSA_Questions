6f378dd7a144ff341a5e631e637179ed01c6d6bc
BhimsenK <kulkarnibhimsen4@gmail.com>
2024-03-12 20:42:00 +0530

    Adding 12th March solved questions.

---
+++ Week 3/12th March/12. Lower.py
@@ -0,0 +1,12 @@
+'''
+Leet code : 709. To Lower Case
+
+Link : https://leetcode.com/problems/to-lower-case/
+
+:) I was too tired to solve any medium question today so I just solved this just to be consistent.
+'''
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
